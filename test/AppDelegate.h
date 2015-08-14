fb11fb1a01f565e99125495975f056ce3bdaf42f
HuaMi <appple@apppletekiMacBook-Pro.local>
2015-08-14 15:55:55 +0800

    create test

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by HuaMi on 15/8/14.
+//  Copyright (c) 2015年 com.wally. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
